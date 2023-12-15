90c6c96339ce400cf7caa8b48789b57711a73142
ronnie-samaroo <ron.sam624@gmail.com>
2023-12-15 01:10:06 -0600

    feat: add new problem

--- pages/recruiter/1_Create_New_Test.py
+++ pages/recruiter/1_Create_New_Test.py
@@ -1,9 +1,13 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
+from streamlit_pills import pills
 from st_pages import show_pages, Page, hide_pages
+
+from google.cloud import firestore
 
 from utils.init import initialize_app
 from utils.components import sidebar_logout
+from utils.db import db
 
 
 def create_new_test():
@@ -27,12 +31,66 @@ def create_new_test():
     # Header
     st.header("Create New Test")
     
-    
+    # Form
+    with st.form("new_test_form", border=False):
+        problems = []
+        selected_topic = pills(
+            "Select a test topic",
+            options=[
+                "Node JS", "Python", "Machine Learning & Deep Learning", "Java", "Javascript", "ASP.NET", "C#",
+                "PHP", "MY SQL", "SQL Server", "GOLang", "C++", "Data Structures & Algorithms","Ruby & Rails",
+                "Rust", "LangChain", "llamaIndex", "Object Oriented Programming", "Unity", "Swift", "Objective-C"
+            ],
+            index=-1
+        )
+        
+        col1, col2 = st.columns([1, 1])
+
+        with col1:
+            with st.container(border=True):
+                st.subheader("Problems")
+
+        with col2:
+            with st.container(border=True):
+                st.subheader("Select Problems")
+                tabs = st.tabs(["Add New Problem", "Select From Existing Problems"])
+                with tabs[0]:
+                    new_problem_description = st.text_area("Description")
+                    new_problem_category = st.selectbox("Category", ("Basic", "Algorithm", "Practice"))
+                    if st.form_submit_button("Save and Add", type="primary"):
+                        if not selected_topic:
+                            st.error("Select a test topic")
+                        elif not new_problem_description:
+                            st.error("Description should not be empty.")
+                        elif not new_problem_category:
+                            st.error("Select a category")
+                        else:
+                            with st.spinner("Saving..."):
+                                try:
+                                    db.collection("problems").add({
+                                        "topic": selected_topic,
+                                        "description": new_problem_description,
+                                        "category": new_problem_category,
+                                        "created_at": firestore.SERVER_TIMESTAMP,
+                                        "creator": "test_user_id"
+                                    })
+                                    st.success("Successfully saved")
+                                except Exception as e:
+                                    st.error(f"Failed to save: {e}")
+                        
+                with tabs[1]:
+                    st.write('hi')
+            
+        st.form_submit_button("Save Test", type="primary")
+
+
 # Run the Streamlit app
 if __name__ == '__main__':
     initialize_app()
     
-    if st.session_state.is_authenticated and st.session_state.role == "recruiter":
-        create_new_test()
-    else:
-        switch_page('home')
+    create_new_test()
+
+    # if st.session_state.is_authenticated and st.session_state.role == "recruiter":
+    #     create_new_test()
+    # else:
+    #     switch_page('home')
