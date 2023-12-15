d2b0b29cf929f7e4d98a789c280424fec6a340c6
ronnie-samaroo <ron.sam624@gmail.com>
2023-12-14 22:47:41 -0600

    fix: sign out button width

--- utils/components.py
+++ utils/components.py
@@ -4,7 +4,8 @@ from streamlit_extras.switch_page_button import switch_page
 
 def sidebar_logout():
     with st.sidebar:
-        clicked = st.button("Sign out", use_container_width=True)
+        col1, col2 = st.columns([1, 2])
+        clicked = col1.button("Sign out", use_container_width=True)
         if clicked:
             st.session_state.is_authenticated = False
             switch_page('home')
