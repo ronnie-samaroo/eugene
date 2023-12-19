d50c9ee9f45d49abcf310eebe175867bdef6862e
ronnie-samaroo <ron.sam624@gmail.com>
2023-12-19 23:50:10 +0100

    feat: added test for prompt engineering

---
+++ ai/test.py
@@ -0,0 +1,53 @@
+from code_test_agent import assess_code
+
+
+problem = "Build a Python client library for interacting with a third-party API. Handle authentication, error handling, and response parsing. Implement data validation and transformation based on API specifications. Design a caching mechanism to optimize performance and reduce API calls. Make the client library modular and easy to integrate into existing applications."
+
+code = """# Here's an example of how you might structure a Python client library for a third-party API
+
+import requests
+import json
+import time
+
+class ThirdPartyAPIClient:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = 'https://api.thirdparty.com'
+        self.cache = {}
+
+    def _make_request(self, endpoint, params=None):
+        headers = {
+            'Authorization': f'Bearer {self.api_key}',
+            'Content-Type': 'application/json'
+        }
+        response = requests.get(f'{self.base_url}/{endpoint}', headers=headers, params=params)
+        return response.json()
+
+    def get_data(self, resource_id):
+        if resource_id in self.cache:
+            return self.cache[resource_id]
+        
+        data = self._make_request(f'resource/{resource_id}')
+        self.cache[resource_id] = data
+        return data
+
+    def validate_data(self, data):
+        # Implement data validation based on API specifications
+        pass
+
+    def transform_data(self, data):
+        # Implement data transformation based on API specifications
+        pass
+
+# Example usage
+api_key = 'your_api_key'
+client = ThirdPartyAPIClient(api_key)
+resource_data = client.get_data('resource123')
+validated_data = client.validate_data(resource_data)
+transformed_data = client.transform_data(validated_data)
+print(transformed_data)
+"""
+
+explanation = "In the above code, I create a ThirdPartyAPIClient class that handles authentication, error handling, response parsing, data validation, transformation, and caching. The get_data method fetches data from the API and caches it to optimize performance. The validate_data and transform_data methods can be implemented based on the API specifications. This client library is designed to be modular and easy to integrate into existing applications."
+
+print(assess_code(problem, code, explanation))
